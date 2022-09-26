4ee8e042eb97f9128119cf3d8936082f5b313290
keith.becker <keith.becker@afs.com>
2022-09-26 19:11:28 +0000

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,2 +1,1 @@
-
-dkp push image-bundle --image-bundle konvoy_image_bundle_v2.3.0_linux_amd64.tar --to-registry http://localhost:5000 --to-registry-insecure-skip-tls-verify
+./dkp push image-bundle --image-bundle konvoy_image_bundle_v2.3.0_linux_amd64.tar --to-registry localhost:5000 --to-registry-insecure-skip-tls-verify
